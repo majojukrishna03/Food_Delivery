836004a5883dcf9eba29b1aca49434aba2e59a38
majojukrishna03 <majojukrishna03@gmail.com>
2024-10-11 22:26:58 +0530

    added app.js and package.json files

--- food_delivery/src/App.js
+++ food_delivery/src/App.js
@@ -1,12 +1,35 @@
 import './App.css';
-import Header from './Components/Header';
-import Footer from './Components/Footer';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, BrowserRouter } from 'react-router-dom';
+import Homepage from './Components/Homepage';
+import ContactUs from './ContactUs';
+import Restaurant from './Components/Restaurant';
+import Menu from './Components/Menu';
+import Cart from './Components/Cart';
+import Checkout from './Components/Checkout';
+import PrivacyPolicy from './PrivacyPolicy';
+import TermsOfService from './TermsOfService';
 
-function App() {
+
+const App = () => {
   return (
-    <><Header/>
-    <Footer/>
-    </>
+    <BrowserRouter>
+      {/* <Router> */}
+      <Routes>
+        <Route path='/' element={<Restaurant />} />
+        <Route path='/about' element={<Homepage />} />
+        <Route path='/contact' element={<ContactUs />} />
+        <Route path='/privacy' element={<PrivacyPolicy/>} />
+        <Route path='/terms' element={<TermsOfService/>} />
+        {/* <Route path='/login' element={<login />} /> */}
+        {/* <Route path='/register' element={<Register />} /> */}
+        <Route path="/restaurant/:id" element={<Menu Restaurant={Restaurant}/>} />
+        <Route path="/cart" element={<Cart />} /> {/* Add the cart route */}
+        <Route path="/checkout" element={<Checkout />} />
+
+      </Routes>
+      {/* </Router> */}
+    </BrowserRouter>
   );
 }
 
